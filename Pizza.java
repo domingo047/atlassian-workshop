89115bb459bfd9fa413f278c2a09380a3ec953bd
domingo047 <161213011+domingo047@users.noreply.github.com>
2024-09-05 04:12:18 +0000

    Add an even cooler line to my file

--- Pizza.java
+++ Pizza.java
@@ -1,5 +1,8 @@
 public class Pizza {
     public static void main(String[] args) throws Exception{
         System.out.println("Pizza is good.");
+        System.out.println("I don't know what pizza is.");
+
+        
     }
 }
