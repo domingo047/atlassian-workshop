eafb41308b0a4b447396bd541a0cb0d5c8e5e869
domingo047 <161213011+domingo047@users.noreply.github.com>
2024-09-05 04:08:48 +0000

    Add new other line to file Pizza.java

--- Pizza.java
+++ Pizza.java
@@ -1,5 +1,6 @@
 public class Pizza {
     public static void main(String[] args) throws Exception{
         System.out.println("Pizza is good.");
+        System.out.println("Pineapple on Pizza is good too.");        
     }
 }
